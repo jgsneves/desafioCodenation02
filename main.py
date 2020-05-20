bc8872654ffaef6cbf247ee82f9ef80f759634af
João Neves <joao.neves@enecad.com.br>
2020-05-20 18:42:37 -0300

    first version

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Employee:
         pass
 
     def get_hours(self):
-        pass
+        return 6
 
 
 class Manager(Employee):
@@ -27,12 +27,30 @@ class Manager(Employee):
     def calc_bonus(self):
         return self.salary * 0.15
 
+    def get_department(self):
+        return self.__departament
+
+    def set_department(self, name, code):
+        self.__departament = Department(name, code)
+
 
 class Seller(Manager):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
         self.departament = Department('sellers', 2)
-        self.sales = 0
+        self.__sales = 0
 
-    def get_hours(self):
-        return 6
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, value):
+        self.__sales = self.__sales + value
+
+    def get_department(self):
+        return self.__departament
+
+    def set_department(self, name, code):
+        self.__departament = Department(name, code)
+
+    def calc_bonus(self):
+        return self.__sales * 0.15
