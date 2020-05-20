13f76727dd49a3be34039c32157664f1e3a3400f
João Neves <joao.neves@enecad.com.br>
2020-05-20 18:59:43 -0300

    removed one underscore in atributes

--- main.py
+++ main.py
@@ -22,35 +22,36 @@ class Employee:
 class Manager(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.__departament = Department('managers', 1)
+        self._departament = Department('managers', 1)
 
     def calc_bonus(self):
         return self.salary * 0.15
 
     def get_department(self):
-        return self.__departament
+        return self._departament
 
     def set_department(self, name, code):
-        self.__departament = Department(name, code)
+        self._departament = Department(name, code)
 
 
 class Seller(Manager):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.__sales = 0
+        self._departament = Department('sellers', 2)
+        self._sales = 0
 
     def get_sales(self):
-        return self.__sales
+        return self._sales
 
     def put_sales(self, value):
-        self.__sales = self.__sales + value
+        self._sales = self._sales + value
 
     def get_department(self):
-        return self.__departament
+        return self._departament
 
     def set_department(self, name, code):
-        self.__departament = Department(name, code)
+        self._departament = Department(name, code)
 
     def calc_bonus(self):
-        return self.__sales * 0.15
+        return self._sales * 0.15
+
